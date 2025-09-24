49877392f6acd1749d2ffa7e6786ae18492ba876
animesh65432 <kiranduttta234@gmail.com>
2025-09-24 15:02:43 +0530

    fix it

--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -1,3 +1,5 @@
+type Env = "PROD" | "dev";
+
 export const API_BASE_URL = "https://indiagovspeak.vercel.app"
-export const env = "dev"
+export const env: Env = process.env.NODE_ENV === "production" ? "PROD" : "dev";
 export const LocalhostUrl = "http://127.0.0.1:8000"
