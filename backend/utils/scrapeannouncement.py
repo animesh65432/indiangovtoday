cd084c4b4534c311335486b82b2cd55fd66e9e49
animesh65432 <kiranduttta234@gmail.com>
2025-09-25 11:44:55 +0530

    fix

--- backend/utils/scrapeannouncement.py
+++ backend/utils/scrapeannouncement.py
@@ -1,31 +1,39 @@
 from models.announcement import Announcement
 import requests
 from bs4 import BeautifulSoup
+from config import config
+
+API_KEY = config["SCRAPERAPI"]  
 
 def scrapeannouncement(BASE_URL: str) -> Announcement | None:
     try:
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                          "AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/117.0 Safari/537.36"
-        }
-        resp = requests.get(BASE_URL, headers=headers)
-        resp.raise_for_status()
+       
+        resp = requests.get(
+            f"https://api.scraperapi.com?api_key={API_KEY}&url={BASE_URL}&render=true",
+            timeout=30
+        )
+        resp.raise_for_status() 
 
         soup = BeautifulSoup(resp.text, "html.parser")
 
+     
         main_container = soup.find("div", class_="innner-page-main-about-us-content-right-part")
         if not main_container:
             return None
 
+       
         title_tag = main_container.find("h2", id="Titleh2")
         title = title_tag.get_text(strip=True) if title_tag else None
 
+       
         paragraphs = main_container.find_all("p")
         content = " ".join(p.get_text(" ", strip=True) for p in paragraphs if p.get_text(strip=True))
 
-        return Announcement(title=title,content=content)
+        return Announcement(title=title, content=content)
 
+    except requests.RequestException as e:
+        print(f"Error fetching the webpage: {e}")
+        return None
     except Exception as e:
         print(f"Scraping failed: {e}")
         return None
