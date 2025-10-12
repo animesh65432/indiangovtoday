4ca62963b99ff9d5d98b79849395a4e917aa9291
animesh65432 <kiranduttta234@gmail.com>
2025-10-12 14:11:38 +0530

    add title make importent

--- frontend/components/Home/Announcement.tsx
+++ frontend/components/Home/Announcement.tsx
@@ -21,8 +21,8 @@ const Announcement: React.FC<Props> = ({ Announcement }) => {
     return (
         <div className='p-2 flex flex-col sm:flex-row gap-4 sm:gap-0 sm:items-center rounded-md justify-between w-[97%] bg-[#FFFFFF] pb-3'>
             <div className='flex flex-col gap-2'>
-                <div className='text-[#E0614B] font-medium text-[1.1rem]'>{Announcement.type}</div>
-                <div className='text-[#2B2B2B] text-[0.9rem]'>{Announcement.title}</div>
+                <div className='text-[#E0614B] font-medium text-[1.1rem]'>{Announcement.title}</div>
+                <div className='text-[#2B2B2B] text-[0.9rem]'>{Announcement.type}</div>
             </div>
             <Button onClick={() => redirect_to(Announcement._id)} className='bg-[#FFFFFF] ml-auto border border-[#E0614B] text-[#E0614B] rounded-xl w-[120px] hover:bg-[#FFFFFF]  cursor-pointer'>{TranslateText[language].See_Details}</Button>
         </div>
