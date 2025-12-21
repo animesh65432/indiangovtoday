4de11980d69fa27bd4d55e51675d1f89841a5ad8
animesh65432 <kiranduttta234@gmail.com>
2025-12-21 16:02:01 +0530

    delete python

--- backend/Schema/SavesModel.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class SaveModel(BaseModel):
-    id : str
-    UserId : str 
-    lan : str
-    link : str
-    
-    
--- backend/models/Indianannouncements.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class IndianannouncementModel(BaseModel):
-    id:str
-    target_lan: str
-    
