32b877637d2847867814a63fc1ec85cfeacf0876
animesh65432 <kiranduttta234@gmail.com>
2026-01-07 02:46:37 +0530

    update it

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'indgov.s3.ap-south-1.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
